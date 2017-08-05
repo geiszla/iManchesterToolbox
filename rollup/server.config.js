bbf8f537954b387505d6ac371f643627b62c05d2
András Geiszl <geiszla@gmail.com>
2017-08-05 16:04:43 +0200

    Deployment api url on server

--- rollup/server.config.js
+++ rollup/server.config.js
@@ -1,11 +1,15 @@
 const babel = require('rollup-plugin-babel');
 const commonjs = require('rollup-plugin-commonjs');
+const replace = require('rollup-plugin-replace');
 
 const ignoredWarnings = ['MISSING_GLOBAL_NAME', 'MISSING_NODE_BUILTINS', 'UNRESOLVED_IMPORT'];
 
 export default {
   entry: 'server/server.jsx',
   plugins: [
+    replace({
+      API_URL: "process.env.NODE_ENV === 'production' ? 'https://imant.herokuapp.com/api' : 'https://localhost/api'"
+    }),
     commonjs({
       sourceMap: false
     }),
